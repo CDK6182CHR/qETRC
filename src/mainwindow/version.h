8b7d68097c404f5a38a815c25a89b7361a885bd0
CDK6182CHR <794288384@qq.com>
2022-08-12 22:47:36 +0800

    2022.08.12  V1.1.11-R25 <此版本见证与jilim一起的第一个月~>

--- src/mainwindow/version.h
+++ src/mainwindow/version.h
@@ -4,7 +4,7 @@
 
 namespace qespec {
 	inline constexpr std::string_view TITLE = "qETRC";
-	inline constexpr std::string_view VERSION = "V1.1.10";
-    inline constexpr std::string_view DATE = "20220605";
-	inline constexpr int RELEASE_CODE = 24;
+	inline constexpr std::string_view VERSION = "V1.1.11";
+    inline constexpr std::string_view DATE = "20220812";
+	inline constexpr int RELEASE_CODE = 25;
 }
