9a0531e74d00280f8a569b19b518bac4db5bfa0c
xep <mxy0268@qq.com>
2023-01-27 13:12:42 +0800

    2023.01.27  V1.3.1-R35

--- src/mainwindow/version.cpp
+++ src/mainwindow/version.cpp
@@ -4,12 +4,11 @@ namespace qespec {
 
 const std::string_view VERSION = "V1.3.1";
 const std::string_view LAST_VERSION = "V1.3.0";
-const std::string_view DATE = "20230124";
-const int RELEASE_CODE = 34;
+const std::string_view DATE = "20230127";
+const int RELEASE_CODE = 35;
 
 const std::string_view DOC_URL = "https://qetrc.readthedocs.io";
 
-const std::string_view UPDATE_LOG = R"(1. 重新设计列车筛选器相关功能；
-2. 新增列车运行统计功能；
-3. 明确标尺排图/贪心推线中的营业站设置规则。)";
+const std::string_view UPDATE_LOG = R"(1. 修正列车筛选器系统的一些细节或者问题；
+2. 修正单双线判断的逻辑问题。)";
 }
