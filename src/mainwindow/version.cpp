9a5cbb71633678f81fbb14f2a41b55bb84db4a10
xep <mxy0268@qq.com>
2023-05-19 22:53:19 +0800

    2023.05.19  V1.3.4 R38

--- src/mainwindow/version.cpp
+++ src/mainwindow/version.cpp
@@ -2,12 +2,12 @@
 
 namespace qespec {
 
-const std::string_view VERSION = "V1.3.3";
-const std::string_view LAST_VERSION = "V1.3.2";
-const std::string_view DATE = "20230218";
-const int RELEASE_CODE = 37;
+const std::string_view VERSION = "V1.3.4";
+const std::string_view LAST_VERSION = "V1.3.3";
+const std::string_view DATE = "20230519";
+const int RELEASE_CODE = 38;
 
 const std::string_view DOC_URL = "https://qetrc.readthedocs.io";
 
-const std::string_view UPDATE_LOG = R"(新增快速推定功能，用于一键推定当前列车在当前线路的通过站时刻。)";
+const std::string_view UPDATE_LOG = R"(放宽交路图功能的限制条件。)";
 }
