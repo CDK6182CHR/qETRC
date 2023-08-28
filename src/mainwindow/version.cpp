5ca8c571afece2cf18894df6b06902b8d64db528
xep <mxy0268@qq.com>
2023-08-28 22:27:17 +0800

    2023.08.28  V1.5.0  R43

--- src/mainwindow/version.cpp
+++ src/mainwindow/version.cpp
@@ -4,12 +4,13 @@ namespace qespec {
 
 const std::string_view VERSION = "V1.5.0";
 const std::string_view LAST_VERSION = "V1.4.0";
-const std::string_view DATE = "20230823";
-const int RELEASE_CODE = 42;
+const std::string_view DATE = "20230828";
+const int RELEASE_CODE = 43;
 
 const std::string_view DOC_URL = "https://qetrc.readthedocs.io";
 const std::string_view DOC_URL_PREFIX = "https://qetrc.readthedocs.io/zh_CN/latest";
 
 const std::string_view UPDATE_LOG = R"(1. 【重要更新】新增列车径路数据项，支持相关数据管理及按径路铺画运行线。
-2. 新增文本输出窗口和铺画问题列表窗口，默认浮动于窗口下方。)";
+2. 新增文本输出窗口和铺画问题列表窗口，默认浮动于窗口下方。
+3. 在标尺排图功能中，新增自动推断可能的线路。)";
 }
