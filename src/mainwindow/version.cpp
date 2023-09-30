4938e3182eb8444ca5fbcae9aa1790197f5d0a97
xep <mxy0268@qq.com>
2023-09-30 13:11:43 +0800

    2023.09.30  V1.5.2 R45

--- src/mainwindow/version.cpp
+++ src/mainwindow/version.cpp
@@ -2,14 +2,15 @@
 
 namespace qespec {
 
-const std::string_view VERSION = "V1.5.1";
-const std::string_view LAST_VERSION = "V1.5.0";
-const std::string_view DATE = "20230912";
-const int RELEASE_CODE = 44;
+const std::string_view VERSION = "V1.5.2";
+const std::string_view LAST_VERSION = "V1.5.1";
+const std::string_view DATE = "20230930";
+const int RELEASE_CODE = 45;
 
 const std::string_view DOC_URL = "https://qetrc.readthedocs.io";
 const std::string_view DOC_URL_PREFIX = "https://qetrc.readthedocs.io/zh_CN/latest";
 
-const std::string_view UPDATE_LOG = R"(1. 在列车管理中新增技术速度项目。
-2. 修复列车径路、交路等功能相关的问题。)";
+const std::string_view UPDATE_LOG = R"(1. 修复批量复制车次中重复车次检测问题。
+2. 修复部分情况下，选中含交路列车导致程序崩溃问题。
+3. 修复导入按标尺排图的线路的相关问题。)";
 }
