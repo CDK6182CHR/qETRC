8aaf720eaa4d1c2c653ee3e1a60186d052fff31e
xep <mxy0268@qq.com>
2023-12-21 22:07:47 +0800

    2023.12.21  V1.5.3, R45; also fix Github issue #3

--- src/mainwindow/version.cpp
+++ src/mainwindow/version.cpp
@@ -2,15 +2,14 @@
 
 namespace qespec {
 
-const std::string_view VERSION = "V1.5.2";
-const std::string_view LAST_VERSION = "V1.5.1";
-const std::string_view DATE = "20230930";
-const int RELEASE_CODE = 45;
+const std::string_view VERSION = "V1.5.3";
+const std::string_view LAST_VERSION = "V1.5.2";
+const std::string_view DATE = "20231221";
+const int RELEASE_CODE = 46;
 
 const std::string_view DOC_URL = "https://qetrc.readthedocs.io";
 const std::string_view DOC_URL_PREFIX = "https://qetrc.readthedocs.io/zh_CN/latest";
 
-const std::string_view UPDATE_LOG = R"(1. 修复批量复制车次中重复车次检测问题。
-2. 修复部分情况下，选中含交路列车导致程序崩溃问题。
-3. 修复导入按标尺排图的线路的相关问题。)";
+const std::string_view UPDATE_LOG = R"(1. 将SARibbon支持库版本升级至1.0.7。
+2. 修复部分已知问题。)";
 }
