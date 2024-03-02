cae92803a69e42f6f6430b7b33d2ae42e3fe68a8
xep <mxy0268@qq.com>
2024-03-02 13:43:09 +0800

    2024.03.02 release: V1.6.0 R49

--- src/mainwindow/version.cpp
+++ src/mainwindow/version.cpp
@@ -2,17 +2,16 @@
 
 namespace qespec {
 
-const std::string_view VERSION = "V1.5.5";
-const std::string_view LAST_VERSION = "V1.5.4";
-const std::string_view DATE = "20240213";
-const int RELEASE_CODE = 48;
+const std::string_view VERSION = "V1.6.0";
+const std::string_view LAST_VERSION = "V1.5.5";
+const std::string_view DATE = "20240302";
+const int RELEASE_CODE = 49;
 
 const std::string_view DOC_URL = "https://qetrc.readthedocs.io";
 const std::string_view DOC_URL_PREFIX = "https://qetrc.readthedocs.io/zh_CN/latest";
 
-const std::string_view UPDATE_LOG = R"(1. 新增在贪心推线功能中，固定指定站停车时间不变。
-2. 在标尺排图、贪心推线中，新增批量设置停点功能。
-3. 在标尺排图、贪心推线中，新增点击铺画点查看/调整停车时间。
-4. 新增标尺数据到特定格式CSV文件的导入/导出。
-5. 一些细节和问题修复。)";
+const std::string_view UPDATE_LOG = R"(1. 调整运行线铺画方式。与此前版本相比，相同设置项下的运行线宽度会减小。可在类型管理器中调整。
+2. 新增浮动交路连线、交路连线显示选项。新增交路连线可选标注车次或交路名。
+3. 新增车次标注形式选项，并新增交路连线模式及相关设置项。
+4. 优化交路中的虚拟车次、不完整时间范围运行图的铺画等。)";
 }
