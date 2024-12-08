9fc92fcd7c028f0a27bfd8caf40e6283710ecf5f
xep <mxy0268@qq.com>
2024-12-08 15:32:20 +0800

    2024-12-08 15:32:10   V1.7.6  R59

--- src/mainwindow/version.cpp
+++ src/mainwindow/version.cpp
@@ -4,14 +4,14 @@
 namespace qespec {
 
 const std::string_view VERSION = QETRC_VERSION;
-const std::string_view LAST_VERSION = "V1.7.4";
-const std::string_view DATE = "20240804";
-const int RELEASE_CODE = 58;
+const std::string_view LAST_VERSION = "V1.7.5";
+const std::string_view DATE = "20241208";
+const int RELEASE_CODE = 59;
 
 const std::string_view DOC_URL = "https://qetrc.readthedocs.io";
 const std::string_view DOC_URL_PREFIX = "https://qetrc.readthedocs.io/zh_CN/latest";
 
-const std::string_view UPDATE_LOG = R"(1. 新增拆分交路、合并交路功能。
-2. 新增交路里程查看功能。
-3. 新增选择交路页面双击提交。)";
+const std::string_view UPDATE_LOG = R"(1. 新增自动上下边距功能。
+2. 允许在速览时刻/速览信息窗口中查阅正在铺画列车的时刻及信息。
+3. 更新支持库版本，及细节修复或优化。)";
 }
