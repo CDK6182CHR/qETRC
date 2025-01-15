820015a2a5bd7cdf579e719845d23d9ebe198480
xep <mxy0268@qq.com>
2025-01-15 23:23:34 +0800

    2025-01-15 23:23:24  V1.7.7 R60

--- src/mainwindow/version.cpp
+++ src/mainwindow/version.cpp
@@ -4,14 +4,13 @@
 namespace qespec {
 
 const std::string_view VERSION = QETRC_VERSION;
-const std::string_view LAST_VERSION = "V1.7.5";
-const std::string_view DATE = "20241208";
-const int RELEASE_CODE = 59;
+const std::string_view LAST_VERSION = "V1.7.6";
+const std::string_view DATE = "20250115";
+const int RELEASE_CODE = 60;
 
 const std::string_view DOC_URL = "https://qetrc.readthedocs.io";
 const std::string_view DOC_URL_PREFIX = "https://qetrc.readthedocs.io/zh_CN/latest";
 
-const std::string_view UPDATE_LOG = R"(1. 新增自动上下边距功能。
-2. 允许在速览时刻/速览信息窗口中查阅正在铺画列车的时刻及信息。
-3. 更新支持库版本，及细节修复或优化。)";
+const std::string_view UPDATE_LOG = R"(1. 修复断面事件表中，运行线跨日导致的错误。
+2. 更新SARibbon版本至2.2.6。)";
 }
